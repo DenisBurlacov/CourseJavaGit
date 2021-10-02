97c0ebf5a36e8bd6e0eff29d07399b02c2746b88
Burlakov Denys <denis055023@gmail.com>
2021-10-02 09:48:54 +0300

    test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        int test = 12;
+        String test2 = "test";
+        System.out.println(test2);
     }
 }
