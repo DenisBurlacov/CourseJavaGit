f37bec7062b3170e750b1928418152d0440ad2d2
Burlakov Denys <denis055023@gmail.com>
2021-10-02 10:17:37 +0300

    test6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int test = 12;
         String test2 = "test";
-        System.out.println(test2);
-        System.out.println(test2);
+
         System.out.println(test2);
         System.out.println(test2);
         System.out.println(test2);
